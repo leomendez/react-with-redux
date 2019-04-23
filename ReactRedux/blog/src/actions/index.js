f3d90b7a15bb41e1e1c3c8c94b229c5c93198803
Leo <leomenbel93@gmail.com>
2019-04-23 08:03:28 -0700

    lecture 187

--- ReactRedux/blog/src/actions/index.js
+++ ReactRedux/blog/src/actions/index.js
@@ -1,18 +1,29 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async dispatch => {
+    await dispatch(fetchPosts());
+    console.log('fetched posts');
+};
+
 export const fetchPosts = () => async dispatch => {
     const response =  await jsonPlaceholder.get('/posts');
     
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => dispatch => {
-   _fetchUser(id, dispatch);
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
 }
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = (id) => dispatch => {
+//    _fetchUser(id, dispatch);
+// }
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
     
-    dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
